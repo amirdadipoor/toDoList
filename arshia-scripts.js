7b76a62a738d4a66a4bef1160d3849098b0a5dbf
Amir Dadipoor <amirdadipoor>
2025-03-01 21:00:05 +0330

    modify arshia codes

--- arshia-scripts.js
+++ arshia-scripts.js
@@ -1,26 +1,36 @@
 let inputBox = document.getElementById("input-box");
 let listContainer = document.getElementById("list-container");
 function addTask(){
-    if(inputBox.value === ''){
-        alert("add something")
-    }else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span =document.createElement('span');
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+
+    let task = inputBox.value.trim();
+
+    if(task === '') {
+        alert("add something");
+        return;
     }
+    appendTaskToList(task, false);
+
     inputBox.value="";
-    saveData();
+    //saveData();
+    addNewTaskToLocalStorage(task);
 }
 listContainer.addEventListener("click", function (e) {
+
+    //console.log(e.target.tagName);
+    let li = e.target.closest("li");
+    let ul = li.parentElement;
+    let index = Array.from(ul.children).indexOf(li);
+
+    //console.log(index);
+
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        saveData();
+        //saveData();
+        toggleTaskState(index)
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData();
+        //saveData();
+        deleteTaskFromLocalStorage(index);
     }
 }, false);
 function saveData(){
@@ -31,4 +41,48 @@ function showTask(){
     listContainer.innerHTML =localStorage.getItem("data");
 
 }
-showTask();
+
+const appendTaskToList = (task , state ) => {
+    let li = document.createElement("li");
+    li.innerHTML = task;
+    state === false ? li.classList.remove("checked") : li.classList.add("checked");
+    listContainer.appendChild(li);
+    let span =document.createElement('span');
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+}
+
+const initAppLocalStorage = () => {
+    let appLocalStorage = JSON.parse(localStorage.getItem("data")) || [] ;
+
+    appLocalStorage.forEach((item) => {
+        //console.log(item);
+        appendTaskToList(item.task, item.done);
+    })
+}
+
+
+
+const addNewTaskToLocalStorage = (task) => {
+    let appLocalStorage = JSON.parse(localStorage.getItem("data")) || [] ;
+
+    const newTask = {task, done : false};
+    appLocalStorage.push(newTask);
+    localStorage.setItem("data", JSON.stringify(appLocalStorage) );
+}
+
+const toggleTaskState = (index) => {
+    let myTasks = JSON.parse(localStorage.getItem("data"));
+    myTasks[index].done = !myTasks[index].done;
+    localStorage.setItem("data", JSON.stringify(myTasks));
+}
+
+const deleteTaskFromLocalStorage = (index) => {
+    let myTasks = JSON.parse(localStorage.getItem("data"));
+    myTasks.splice(index, 1);
+    localStorage.setItem("data", JSON.stringify(myTasks));
+
+}
+
+initAppLocalStorage();
+//showTask();
--- white-board.js
+++ white-board.js
@@ -1,11 +1,19 @@
-let storage = localStorage.getItem("tasks");
-console.log(storage);
-if (storage === null || storage === undefined) {
-    storage = new Map();
-}
+
+
 let myTasks = [
-    {task : "task1" , done: false},
-    {task : "task2" , done: false},
-    {task : "task3" , done: false},
-    {task : "task4" , done: true},
-]
+    {task : "task1" , done : false} ,
+    {task : "task1" , done : false} ,
+    {task : "task1" , done : false} ,
+    {task : "task1" , done : false} ,
+    {task : "task1" , done : false} ,
+];
+
+
+
+
+// array or object to string
+console.log ( btoa(JSON.stringify(myTasks))  );
+
+
+// str to object or array
+//console.log( JSON.parse(myTasks) );
