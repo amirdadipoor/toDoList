b06600b3a8b02a9ae0d7f8fe815d18fe888b5d95
Amir Dadipoor <amirdadipoor>
2025-03-25 23:22:00 +0330

    build app and install tailwind css 2

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         path : path.resolve(__dirname,'dist'),
         filename: "[name].[contenthash].js",
     },
+    stats: 'normal',
     devServer : {
         static: path.resolve(__dirname, "dist"), // Serve files from "dist"
         port: 8989, // Choose any port
