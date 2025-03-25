2157b6b78900cae0f1b8abba98673bcc7c103a4e
amir dadipoor <amirdadipoor@hotmail.com>
2025-03-25 13:09:57 +0330

    build structure of project with webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+console.log("Amir")
+console.log("dadipoor")
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+    entry : {
+        bundle: "./src/index.js",
+    },
+    mode: "production",
+    cache: false,
+    output : {
+        path : path.resolve(__dirname,'dist'),
+        filename: "[name].js",
+    },
+    devServer : {
+        static: path.resolve(__dirname, "dist"), // Serve files from "dist"
+        port: 8989, // Choose any port
+        open: true, // Automatically open the browser
+        compress: true, // Enable gzip compression
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "ToDo App",
+            template: "./src/index.html",
+            filename: "index.html",
+        }),
+    ],
+}
